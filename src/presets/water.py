6131ebd82fc8ecb13726ca9a5a468d9469f73e91
3n3l <nncnep@gmail.com>
2026-06-11 15:59:11 +0200

    feat: more water presets

--- src/presets/water.py
+++ src/presets/water.py
@@ -7,30 +7,31 @@ water_presets = [
         information="Water",
         dt=1e-3,
         geometries=[
-            *[
-                Rectangle(
-                    material=Water,  # pyright: ignore
-                    is_continuous=True,
-                    frame_threshold=i,
-                    lower_left=(0.47, 0.94, 0.5),
-                    velocity=(0, -2, 0),
-                    size=(0.06, 0.06, 0.6),
-                )
-                for i in range(3, 203)
-            ],
+            # *[
+            #     Rectangle(
+            #         material=Water,  # pyright: ignore
+            #         is_continuous=True,
+            #         frame_threshold=i,
+            #         lower_left=(0.47, 0.94, 0.47),
+            #         velocity=(0, -2, 0),
+            #         size=(0.06, 0.06, 0.6),
+            #     )
+            #     for i in range(3, 203)
+            # ],
             *[
                 Circle(
                     material=Water,  # pyright: ignore
                     is_continuous=True,
                     frame_threshold=i,
-                    center=(0.5, 0.94, 0.5),
-                    velocity=(0, -2, 0),
+                    center=(0.5, 0.985, 0.5),
+                    velocity=(0, -3, 0),
                     radius=0.03,
                 )
                 for i in range(3, 203)
             ],
         ],
     ),
+
     # Configuration(
     #     name="Waterjet & Pool",
     #     information="Water",
@@ -66,45 +67,45 @@ water_presets = [
     #         ],
     #     ],
     # ),
-    # Configuration(
-    #     name="Dam Break",
-    #     information="Water",
-    #     dt=1e-3,
-    #     geometries=[
-    #         Rectangle(
-    #             material=Water,  # pyright: ignore
-    #             lower_left=(0.0, 0.0),
-    #             size=(0.3, 0.4),
-    #             velocity=(0, 0),
-    #         ),
-    #     ],
-    # ),
-    # Configuration(
-    #     name="Dam Break, Centered",
-    #     information="Water",
-    #     dt=1e-3,
-    #     geometries=[
-    #         Rectangle(
-    #             material=Water,  # pyright: ignore
-    #             lower_left=(0.35, 0.0),
-    #             size=(0.3, 0.4),
-    #             velocity=(0, 0),
-    #         ),
-    #     ],
-    # ),
-    # Configuration(
-    #     name="Spherefall, Water",
-    #     information="Water",
-    #     dt=1e-3,
-    #     geometries=[
-    #         Circle(
-    #             material=Water,  # pyright: ignore
-    #             center=(0.5, 0.4),
-    #             velocity=(0, -3),
-    #             radius=0.1,
-    #         ),
-    #     ],
-    # ),
+    Configuration(
+        name="Dam Break",
+        information="Water",
+        dt=1e-3,
+        geometries=[
+            Rectangle(
+                material=Water,  # pyright: ignore
+                lower_left=(0.0, 0.0, 0.0),
+                size=(0.3, 0.3, 1.0),
+                velocity=(0, 0, 0),
+            ),
+        ],
+    ),
+    Configuration(
+        name="Dam Break, Centered",
+        information="Water",
+        dt=1e-3,
+        geometries=[
+            Rectangle(
+                material=Water,  # pyright: ignore
+                lower_left=(0.35, 0.0, 0.0),
+                size=(0.3, 0.3, 1.0),
+                velocity=(0, 0, 0),
+            ),
+        ],
+    ),
+    Configuration(
+        name="Spherefall, Water",
+        information="Water",
+        dt=1e-3,
+        geometries=[
+            Circle(
+                material=Water,  # pyright: ignore
+                center=(0.5, 0.4, 0.5),
+                velocity=(0, -3, 0),
+                radius=0.1,
+            ),
+        ],
+    ),
     # Configuration(
     #     name="Pool",
     #     information="Water",
